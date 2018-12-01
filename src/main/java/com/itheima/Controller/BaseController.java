5ee7d3e29b91be0ac994f51d48c339774eb4ca0b
sublun <sublun@126.com>
2018-12-01 19:19:59 +0800

    修改后提交

--- src/main/java/com/itheima/Controller/BaseController.java
+++ src/main/java/com/itheima/Controller/BaseController.java
@@ -1,6 +1,11 @@
 package com.itheima.Controller;
 
 public class BaseController {
+
     //测试修改
     // https://github.com/jyjdeqingchun/git_test.git
+    public static void main(String[] args) {
+        System.out.println("测试。");
+    }
+
 }
